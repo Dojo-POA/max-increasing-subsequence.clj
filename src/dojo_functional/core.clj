7a740b1e550bc6aca51d008e8b76eb07e19e89a4
Pedro Leal <pedrombl@gmail.com>
2013-08-21 20:34:57 -0300

    refatorando testes e adicionando novo teste case

--- src/dojo_functional/core.clj
+++ src/dojo_functional/core.clj
@@ -5,13 +5,32 @@
   (= nil (second sqnc))
 )
 
-(defn find-increasing-sequence [sqnc] 
+(defn find-increasing-sequence-from-first-element [sqnc] 
   (if (one-element-list? sqnc)
     sqnc
     (if 
       (> (first sqnc) (second sqnc))
       (list (first sqnc))
-      (cons (first sqnc) (find-increasing-sequence (rest sqnc)))
+      (cons (first sqnc) (find-increasing-sequence-from-first-element (rest sqnc)))
     )    
   )
+)
+
+(defn max-list [x1 x2]
+  (if 
+    (>= (count x1) (count x2))
+    x1
+    x2
+  )
+)
+
+(defn find-increasing-sequence [sqnc] 
+  (if (empty? sqnc)
+   (find-increasing-sequence-from-first-element sqnc)
+    (max-list 
+      (find-increasing-sequence-from-first-element sqnc)
+      (find-increasing-sequence (rest sqnc))
+      )
+  )
+  
 )
--- test/dojo_functional/core_test.clj
+++ test/dojo_functional/core_test.clj
@@ -2,36 +2,17 @@
   (:require [clojure.test :refer :all]
             [dojo-functional.core :refer :all]))
 
-(deftest find-increasing-sequence-test
-  (testing "find sequence (1 2 3) inside (1 2 3)"
-    (is 
-      (= 
-          (find-increasing-sequence (list 1 2 3)) (list 1 2 3)
-      )
-    )
-  )
-  
+(deftest find-increasing-sequence-test  
   (testing "is empty"
-    (is 
-      (empty?   
-          (find-increasing-sequence [] ) 
-      )
-    )
+    (is (empty? (find-increasing-sequence [] )))
   )
   
-  (testing "find sequence (2) inside (2 1)"
-    (is 
-      (=   
-          (find-increasing-sequence (list 2 1)) (list 2)
-      ) 
-    )
-  )
-  (testing "find sequence (2 3) inside (2 3 1)"
-    (is 
-      (=   
-          (find-increasing-sequence (list 2 3 1)) (list 2 3)
-      ) 
-    )
+  (testing "find increasing sequence"
+    (is (= (find-increasing-sequence (list 1)) (list 1)))
+    (is (= (find-increasing-sequence (list 2 1)) (list 2)))
+    (is (= (find-increasing-sequence (list 1 2 3)) (list 1 2 3)))
+    (is (= (find-increasing-sequence (list 2 3 1)) (list 2 3)))
+    (is (= (find-increasing-sequence (list 3 1 2 4)) (list 1 2 4)))
   )
 )
     
