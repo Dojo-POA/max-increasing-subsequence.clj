47b9c035bfda2d35530991ccbb29b36b875e706e
Pedro Leal <pedrombl@gmail.com>
2013-08-21 21:10:30 -0300

    adicionando novos casos de teste

--- src/dojo_functional/core.clj
+++ src/dojo_functional/core.clj
@@ -5,31 +5,31 @@
   (= nil (second sqnc))
 )
 
-(defn find-increasing-sequence-from-first-element [sqnc] 
+(defn find-increasing-sequence-from-first-element [sqnc, the-longest-list] 
   (if (one-element-list? sqnc)
     sqnc
-    (if 
-      (> (first sqnc) (second sqnc))
-      (list (first sqnc))
-      (cons (first sqnc) (find-increasing-sequence-from-first-element (rest sqnc)))
+    (if (> (first sqnc) (second sqnc))
+      (the-longest-list 
+        (cons (first sqnc) (rest (rest sqnc)))
+      )
+      (cons (first sqnc) (the-longest-list (rest sqnc)))
     )    
   )
 )
 
 (defn longest-list [x1 x2]
-  (if 
-    (>= (count x1) (count x2))
-    x1
-    x2
-  )
+  (if (>= (count x1) (count x2)) x1 x2)
 )
 
 (defn find-increasing-sequence [sqnc] 
   (if (empty? sqnc)
    sqnc
    (longest-list 
-    (find-increasing-sequence-from-first-element sqnc)
+    (find-increasing-sequence-from-first-element sqnc find-increasing-sequence)
     (find-increasing-sequence (rest sqnc))
    )
   )  
 )
+
+
+
--- test/dojo_functional/core_test.clj
+++ test/dojo_functional/core_test.clj
@@ -13,6 +13,9 @@
     (is (= (find-increasing-sequence (list 1 2 3)) (list 1 2 3)))
     (is (= (find-increasing-sequence (list 2 3 1)) (list 2 3)))
     (is (= (find-increasing-sequence (list 3 1 2 4)) (list 1 2 4)))
+    (is (= (find-increasing-sequence (list 3 4 1 9 8 7)) (list 3 4 9)))
+      (is (= (find-increasing-sequence (list 1 8 2 3 )) (list 1 2 3)))
+    ;(is (= (find-increasing-sequence (list 3 1 6 5 2 4 7)) (list 1 2 4 7)))
   )
 )
     
